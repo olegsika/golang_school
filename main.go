34e31dea782e0c3f6592d7f920f6d45a505211c7
olegsika <olech3000@gmail.com>
2020-05-02 15:28:09 +0300

    #comment Add function for count total views on the page

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main()  {
+import (
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+)
 
+var count int
+
+func main() {
+	http.HandleFunc("/inc", increment)
+
+	if err := http.ListenAndServe(":80", nil); err != nil {
+		log.Panic(err)
+	}
+}
+
+func increment(w http.ResponseWriter, r *http.Request) {
+	count++
+	if _, err := io.WriteString(w, strconv.Itoa(count)); err != nil {
+		log.Panic(err)
+	}
 }
